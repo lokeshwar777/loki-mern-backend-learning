cfb4208af1c5e354d2600b61668b1c272e3feb44
Mallepally Lokeshwar Reddy <lokeshwar777@gmail.com>
2024-01-21 12:11:47 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+// database is in another continent
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MOGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected !! DB HOST : ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection error ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
